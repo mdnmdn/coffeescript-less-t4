3307abc65f357aba7b2f4cd7445a16064fd23620
mdn <marco.denittis@gmail.com>
2012-01-07 19:04:19 +0100

    ajaxmin compress

--- Web1/Content/coffee/coffee1.js
+++ Web1/Content/coffee/coffee1.js
@@ -1,26 +1,2 @@
-﻿/* one.js  */
-var one = 'one.js';
-/* two.coffee  */
-(function() {
-  var two;
-  two = 'two.coffee';
-}).call(this);
-
-/* two.js  */
-var two = 'two.js';
-/* js1.coffee  */
-(function() {
-  alert('ad');
-  alert('ad');
-}).call(this);
-
-/* js2.coffee  */
-(function() {
-  alert('js2');
-  alert('js2asas');
-}).call(this);
-
-/* Three.js  */
-var three = 'three.js';
-
+﻿var one="one.js",two,three;(function(){var n;n="two.coffee"}).call(this),two="two.js",function(){alert("ad"),alert("ad")}.call(this),function(){alert("js2"),alert("js2asas")}.call(this),three="three.js"
 
