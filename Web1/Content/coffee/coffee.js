6a6f299996abdd9c5c9bfcccba2f20cce8e8f7d6
mdn <marco.denittis@gmail.com>
2012-01-07 19:27:44 +0100

    top file list

---
+++ Web1/Content/coffee/coffee.js
@@ -0,0 +1,10 @@
+﻿/*
+one.js
+two.coffee
+two.js
+js1.coffee
+js2.coffee
+Three.js
+*/
+var one="one.js",two,three;(function(){var n;n="two.coffee"}).call(this),two="two.js",function(){alert("ad"),alert("ad")}.call(this),function(){alert("js2"),alert("js2asas")}.call(this),three="three.js"
+
