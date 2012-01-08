2d32db0af63d6db8dcd1ac7df2280ae87e6ccc8d
Marco De Nittis <marco.denittis@gmail.com>
2012-01-08 13:17:06 +0100

    added yui compress libs for futre use

--- Web1/Content/coffee/coffee1.js
+++
@@ -1,2 +0,0 @@
-﻿var one="one.js",two,three;(function(){var n;n="two.coffee"}).call(this),two="two.js",function(){alert("ad"),alert("ad")}.call(this),function(){alert("js2"),alert("js2asas")}.call(this),three="three.js"
-
