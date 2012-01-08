b9bbebc7abdc32704640d6ebf1c39975c53a4293
Marco De Nittis <marco.denittis@gmail.com>
2012-01-08 15:46:26 +0100

    error handling and auto exclusion of the output file

--- Web1/Content/coffee/coffee.js
+++ Web1/Content/coffee/coffee.js
@@ -5,6 +5,70 @@ two.js
 js1.coffee
 js2.coffee
 Three.js
+coffee.js
 */
-var one="one.js",two,three;(function(){var n;n="two.coffee"}).call(this),two="two.js",function(){alert("ad"),alert("ad")}.call(this),function(){alert("js2"),alert("js2asas")}.call(this),three="three.js"
+/* one.js  */
+var one = 'one.js';
+/* two.coffee  */
+(function() {
+  var two;
+  two = 'two.coffee';
+}).call(this);
+
+/* two.js  */
+var two = 'two.js';
+/* js1.coffee  */
+(function() {
+  alert('ad');
+  alert('ad');
+}).call(this);
+
+/* js2.coffee  */
+(function() {
+  alert('js2');
+  alert('js2asas');
+}).call(this);
+
+/* Three.js  */
+var three = 'three.js';
+/* coffee.js  */
+/*
+one.js
+two.coffee
+two.js
+js1.coffee
+js2.coffee
+Three.js
+coffee.js
+*/
+/* one.js  */
+var one = 'one.js';
+/* two.coffee  */
+(function() {
+  var two;
+  two = 'two.coffee';
+}).call(this);
+
+/* two.js  */
+var two = 'two.js';
+/* js1.coffee  */
+(function() {
+  alert('ad');
+  alert('ad');
+}).call(this);
+
+/* js2.coffee  */
+(function() {
+  alert('js2');
+  alert('js2asas');
+}).call(this);
+
+/* Three.js  */
+var three = 'three.js';
+/* coffee.js  */
+
+
+
+
+
 
